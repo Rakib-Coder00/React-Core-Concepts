d7be99630a2f392fb2800b9421633568580a1ab6
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-10-14 13:31:55 +0600

    clear&Copy button added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,6 +2,14 @@ import React, {useState} from "react";
 
 
 export default function TextForm(props) {
+    // const handleCapClick= () =>{
+    //     function capitalize(word) {
+    //         return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    //       }
+    //       let newText = capitalize(text)
+    //       setText(newText)
+    // }
+
     const handleUpClick = () => {
         // console.log(' Uppercase clicked' + text);
         let newText = text.toUpperCase();
@@ -11,6 +19,17 @@ export default function TextForm(props) {
     const handleLoClick = () =>{
         let newText = text.toLowerCase();
         setText(newText)
+
+    }
+    const handleClearClick = () =>{
+        let newText = '';
+        setText(newText)
+
+    }
+    const handleCopy = () =>{
+        let text = document.getElementById('myBox')
+        text.select();
+        navigator.clipboard.writeText(text.value)
 
     }
 
@@ -34,10 +53,16 @@ export default function TextForm(props) {
 
                 </textarea>
             </div>
-            <button className="btn btn-primary" onClick = {handleUpClick}>Convert Uppercase</button>
+            {/* <button className="btn btn-primary mx-2" onClick = {handleCapClick}>Convert Capitalize</button> */}
+            <button className="btn btn-primary mx-2" onClick = {handleUpClick}>Convert Uppercase</button>
 
             <button className="btn btn-primary  mx-2" onClick = {handleLoClick}>Convert Lowercase</button>
+
+            <button className="btn btn-primary  mx-2" onClick = {handleClearClick}>Clear Text</button>
+
+            <button className="btn btn-primary  mx-2" onClick = {handleCopy}>Copy Text</button>
         </div>
+
         <div className="container my-3">
             <h2>Your Text summary</h2>
             <p>{text.split(' ').length} Words and {text.length}</p>
