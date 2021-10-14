12776ab0468d2a374087d28da1d888368ffa6f0e
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-10-14 08:55:06 +0600

    State & Handling Events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
       <Navbar title = 'React Core' aboutTxt = 'About us' />
       {/* <Navbar aboutTxt = 'About us'/> */}
+      <div className="container my-3">
+      <TextForm Heading = 'Enter Heading' TextTitle = 'Enter Text Title'/>
+      </div>
     </>
   );
 }
-
 export default App;
